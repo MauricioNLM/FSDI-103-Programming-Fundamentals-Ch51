2258bfd0354c325a1d2614901a9bad215b0a0973
MauricioNLM <Maurcio.Navarro@sdgku.edu>
2024-08-28 19:42:00 -0700

    A FUNNY STORY (Assignment #1)

---
+++ script/Index.js
@@ -0,0 +1,28 @@
+let character = "Mauricio";
+let action = "run";
+let objetc = "a guitar";
+let place = "the beach";
+let time = "a mornight";
+
+const occasion = "a surprise party";
+
+console.log("--Funny Story--")
+console.log(character + " was at " +place+" on "+time+".")
+console.log("Suddenly "+character+" saw "+objetc+" and decided to "+action+".")
+console.log("It turned out to be "+occasion+" and everyone had a great time!")
+
+
+console.log("--Different Version of the story--")
+
+
+character = "Leo";
+action = "Danced";
+objetc = "a giant rubber duck";
+place = "the parck";
+time = "a sunny afternoon";
+
+console.log("--Funny Story--")
+console.log(character + " was at " +place+" on "+time+".")
+console.log("Suddenly "+character+" saw "+objetc+" and decided to "+action+".")
+console.log("It turned out to be "+occasion+" and everyone had a great time!")
+
