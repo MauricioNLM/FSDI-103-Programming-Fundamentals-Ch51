0e9fac16560c9547f8edec27d7e352a6647c9daa
MauricioNLM <Maurcio.Navarro@sdgku.edu>
2024-09-04 17:50:18 -0700

    Report Final

--- script/ConverTemperature.js
+++ script/ConverTemperature.js
@@ -1,14 +1,42 @@
 function convertTemperature(){
-    let inTemperature = document.getElementById('inputTemperature').value;
+    let startTemperature = document.getElementById('inputStartTemperature').value;
+    let endTemperature = document.getElementById('inputEndTemperature').value;
     let inIndication = document.getElementById('inIndicator').value;
+    let outResult = document.getElementById('outResult');
+    outResult.innerHTML = '';
     let result = '';
-    if(inIndication == 'C'){
-        result = (inTemperature - 32) * 5/9 + " F";
+    let startVal;
+    let endVal;
+
+    if(startTemperature < endTemperature){
+        startVal = startTemperature;
+        endVal = endTemperature;
     }
     else {
-        result = (inTemperature * 9/5) + 32 + " C";
+        startVal = endTemperature ;
+        endVal = startTemperature;
     }
-    let outResult = document.getElementById('outResult');
-    outResult.innerHTML = `<label>Result = `+result+`<\label>`;
+    for(let i = startVal; i<=endVal ; i++){
+
+        if(inIndication == 'C'){
+            result = i+" "+inIndication+" = "+ (i - 32) * 5/9 + " F";
+        }
+        else {
+            result = i+" "+inIndication+" = "+(i * 9/5) + 32 + " C";
+        }
+        outResult.innerHTML = outResult.innerHTML + `<label>Result = `+result+`<\label></br>`;
+    }
+
+    // if(inIndication == 'C'){
+    //     result = (inTemperature - 32) * 5/9 + " F";
+    // }
+    // else {
+    //     result = (inTemperature * 9/5) + 32 + " C";
+    // }
+
+
+
+    // let outResult = document.getElementById('outResult');
+    // outResult.innerHTML = `<label>Result = `+result+`<\label>`;
 
 }
